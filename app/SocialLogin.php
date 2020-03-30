a8bdd0c8803d1f6ae59ab1acc1e8b005dce508e0
SalmaMeniawy <salmamostafameniawy@gmail.com>
2020-03-30 15:14:14 +0200

    add eloquent relation with User model in SocialLogin model

--- app/SocialLogin.php
+++ app/SocialLogin.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\User;
 class SocialLogin extends Model
 {
     protected $fillable = ['user_id', 'provider_user_id', 'provider'];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
